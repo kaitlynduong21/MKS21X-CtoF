9d590e56cd1527a9c1006366c8d704c250d1038d
kduong@stuy.edu <kduong@stuy.edu>
2018-09-19 15:14:31 -0400

    created two functions celsiusToFahrenheit and fahrenheitToCelsius

---
+++ CtoFTester.java
@@ -0,0 +1,8 @@
+public class CtoFTester{
+  public static double celsiusToFahrenheit(double tempC) {
+    return tempC * 1.8 + 32.0;
+  }
+  public static double fahrenheitToCelsius(double tempF) {
+    return (tempF + 32.0) * 5.0 / 9.0;
+  }
+}
