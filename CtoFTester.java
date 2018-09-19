fe5131d0dc6fadfedbbf8c36f580cb17dd5d430e
kduong@stuy.edu <kduong@stuy.edu>
2018-09-19 15:17:37 -0400

    tested my conversion functions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,8 +1,14 @@
 public class CtoFTester{
-  public static double celsiusToFahrenheit(double tempC) {
+  public static double celsiusToFahrenheit(double tempC){
     return tempC * 1.8 + 32.0;
   }
-  public static double fahrenheitToCelsius(double tempF) {
+  public static double fahrenheitToCelsius(double tempF){
     return (tempF + 32.0) * 5.0 / 9.0;
+  }
+  public static void main(String[] args){
+    System.out.println(celsiusToFahrenheit(43.9));
+    System.out.println(celsiusToFahrenheit(-98.5));
+    System.out.println(fahrenheitToCelsius(24.94));
+    System.out.println(fahrenheitToCelsius(-53.851));
   }
 }
